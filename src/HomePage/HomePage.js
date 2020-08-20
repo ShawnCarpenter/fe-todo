67d76e3c000e2f85d478251a970b25d001997099
Shawn Carpenter <shawn.carpeenter@gmail.com>
2020-08-20 09:13:23 -0700

    changed text on home page

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -4,7 +4,7 @@ export default class HomePage extends Component {
   render() {
     return (
       <div>
-        Welvome to your todo list.
+        Welcome to your todo list.
         To get started, login. Once you login you can manage your list.
       </div>
     )
